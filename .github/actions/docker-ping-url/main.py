eb9fa0753a2981a5905c2db43903d52b1854e8d2
Mykyta Kostiuk <kostiuk.mykyta5@google.com>
2025-11-03 10:31:03 +0100

    refactor: reorganize ping_url and run functions for improved readability

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -2,29 +2,39 @@ import time
 import requests
 import os
 
+
+def ping_url(url, delay, max_trials):
+    trials = 0
+    while trials < max_trials:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                return True
+        except requests.ConnectionError:
+            print(
+                f"Website ${url} is unreachable. Retrying in. ${delay} seconds...")
+            time.sleep(delay)
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL format: ${url}.")
+            return False
+    return False
+
+
+def run():
+    url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+
+    accessible = ping_url(url, delay, max_trials)
+    if accessible:
+        print(f"Website ${url} is reachable")
+    else:
+        raise Exception(
+            f"The URL {url} is not accessible after {max_trials} trials.")
+
+ 
+run()
+
 if __name__ == "__main__":
     print("Hello world")
-
-    def ping_url(url, delay, max_trials):
-      trials = 0
-      while trials < max_trials:
-          response = requests.get(url)
-          if response.status_code == 200:
-              return True
-          time.sleep(delay)
-          trials += 1
-      return False
-
-
-    def run():
-        url = os.getenv("INPUT_URL")
-        delay = int(os.getenv("INPUT_DELAY"))
-        max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
-
-        accessible = ping_url(url, delay, max_trials)
-        if accessible:
-            print(f"The URL {url} is accessible.")
-        else:
-            raise Exception(f"The URL {url} is not accessible after {max_trials} trials.")
-
-    run()
